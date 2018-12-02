884185f996ffcd5b2c4c528e77d6cf5c14f30e2e
Nikita018 <43194111+Nikita018@users.noreply.github.com>
2018-12-02 17:46:01 +0530

    Balanced Tress solution
    Balanced Tress solution - HackerRank question

--- HackerRank_solutions/Data Structures/Stacks/Balanced Brackets/Solution.java
+++ HackerRank_solutions/Data Structures/Stacks/Balanced Brackets/Solution.java
@@ -1,69 +1,40 @@
 
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Scanner;
 
-public class Solution {
+public class solution {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n1 = in.nextInt(); //Number of cylinders in first stack
-        int n2 = in.nextInt(); //Number of cylinders in second stack
-        int n3 = in.nextInt(); //Number of cylinders in third stack
-        Queue<Integer> s1 = new LinkedList<Integer>(); //First stack of cylinders
-        Queue<Integer> s2 = new LinkedList<Integer>(); //Second stack of cylinders
-        Queue<Integer> s3 = new LinkedList<Integer>(); //Third stack of cylinders
-        int h1 = 0; //Height of the first stack
-        int h2 = 0; //Height of the second stack
-        int h3 = 0; //Height of the third stack
+        HashMap<Character,Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('{', '}');
+        map.put('[', ']');
         
-        int minStack; //The stack with the smallest height
-        
-        //Initialize the stacks and their heights
-        for(int i = 0; i < n1; i++){
-            int cylinder = in.nextInt();
-            s1.add(cylinder);
-            h1 += cylinder;
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        for(int i=0; i<num ; i++){
+             String expression = scan.next();
+             System.out.println(isBalanced(expression, map) ? "YES" : "NO" );
         }
+        scan.close();
         
-        for(int i = 0; i < n2; i++){
-            int cylinder = in.nextInt();
-            s2.add(cylinder);
-            h2 += cylinder;
-        }
-        
-        for(int i = 0; i < n3; i++){
-            int cylinder = in.nextInt();
-            s3.add(cylinder);
-            h3 += cylinder;
-        }
-        minStack = Math.min(h1,Math.min(h2,h3));//Initialize minStack with the minimum height
-        
-        while(h1 != h2 || h1 != h3)//Heights are not all equal
-        {
-            //Remove cylinders from stack 1 until your height is <= the smallest
-            while(h1 > minStack)
-            {
-                h1 -= s1.remove();
-            }
-            minStack = Math.min(h1,Math.min(h2,h3)); //Recalculate min
-            
-            //Remove cylinders from stack 2 until your height is <= the smallest
-            while(h2 > minStack)
-            {
-                h2 -= s2.remove();
-            }
-            minStack = Math.min(h1,Math.min(h2,h3)); //Recalculate min
-            
-            //Remove cylinders from stack 3 until your height is <= the smallest
-            while(h3 > minStack)
-            {
-                h3 -= s3.remove();
-            }
-            minStack = Math.min(h1,Math.min(h2,h3)); //Recalculate min
-        }
-        System.out.println(minStack);//All stacks are equal so minStack represents their height
+
     }
+    public static boolean isBalanced(String expression, HashMap<Character,Character> map){
+        
+        if(expression.length()%2!=0){
+            return false;
+        }
+        ArrayDeque<Character> deque = new ArrayDeque();
+        for (int i=0; i<expression.length(); i++){
+            Character ch = expression.charAt(i);
+            if(map.containsKey(ch))
+                deque.push(ch);
+            else if(deque.isEmpty() || ch != map.get(deque.pop())){
+                return false;
+            }    
+    }
+ return deque.isEmpty();
+}
 }
